2ea53445641fe4d3ab0fad0da77bfcb1d23c8d94
Pieter Jan De Smedt <pieterjan@digitalcomet.be>
2019-09-02 00:53:40 +0200

    Update '2019-09-02 (maandag) 00:53:40'

--- themes/vite-et-reves/assets/js/scripts.js
+++ themes/vite-et-reves/assets/js/scripts.js
@@ -2,63 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	// NAV BAR HIDE ON SCROLL
 
-	// Code goes here
-	var lastKnownScrollY = 0;
-	var currentScrollY = 0;
-	var ticking = false;
-	var idOfHeader = 'navbar';
-	var eleHeader = null;
 
+	// grab an element
+	var myElement = document.getElementById("navbar");
+	// construct an instance of Headroom, passing the element
+	var headroom  = new Headroom(myElement);
+	// initialise
+	headroom.init({
+		offset: 20,
+		tolerance: 5,
 
-	const classes = {
-		pinned: 'header-pin',
-		unpinned: 'header-unpin',
-	};
-
-
-	function onScroll() {
-		currentScrollY = window.pageYOffset;
-		requestTick();
-	}
-
-
-	function requestTick() {
-		if (!ticking) {
-			requestAnimationFrame(update);
-		}
-		ticking = true;
-	}
-
-	function update() {
-		if (currentScrollY < lastKnownScrollY) {
-			pin();
-		} else if (currentScrollY > lastKnownScrollY) {
-			unpin();
-		}
-		lastKnownScrollY = currentScrollY;
-		ticking = false;
-	}
-
-
-	function pin() {
-		if (eleHeader.classList.contains(classes.unpinned)) {
-			eleHeader.classList.remove(classes.unpinned);
-			eleHeader.classList.add(classes.pinned);
-		}
-	}
-
-
-	function unpin() {
-		if (eleHeader.classList.contains(classes.pinned) || !eleHeader.classList.contains(classes.unpinned)) {
-			eleHeader.classList.remove(classes.pinned);
-			eleHeader.classList.add(classes.unpinned);
-		}
-	}
-
-	window.onload = function () {
-		eleHeader = document.getElementById(idOfHeader);
-		document.addEventListener('scroll', onScroll, false);
-	}
+	});
 
 	// BURGER MENU
 
